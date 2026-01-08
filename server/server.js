57b29a584fe3fe8531638fa574cc27d8f1f6b0fb
Sudhi Kumaran <sudhikumaran2005@gmail.com>
2026-01-08 09:07:47 +0530

    Remove trailing slash from CLIENT_URL in CORS origin matching

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(helmet({
 }));
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL?.split(',').map(url => url.trim()) || ['http://localhost:5173'],
+    origin: process.env.CLIENT_URL?.split(',').map(url => url.trim().replace(/\/$/, '')) || ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
